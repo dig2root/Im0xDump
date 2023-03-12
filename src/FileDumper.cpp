bfda548f0032f3f26a748ff0726b5edd592e9b49
Clément Rault <clem.rault@outlook.com>
2023-03-12 12:51:19 -0400

    Remove data printing in terminal

--- src/FileDumper.cpp
+++ src/FileDumper.cpp
@@ -23,12 +23,4 @@ void FileDumper::dump() {
 	// Read file into buffer
 	m_buffer = new char[m_file_size];
 	m_file.read(m_buffer, m_file_size);
-	
-	// Display file in hexadecimal
-	for (int i = 0; i < m_file_size; i++) {
-		std::cout << std::hex << (int)m_buffer[i] << " ";
-		if (i % 16 == 0 && i != 0) {
-			std::cout << std::endl;
-		}
-	}
 }
